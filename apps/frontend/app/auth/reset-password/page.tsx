41b4ef06a67e55f93f7fe913825247cf9e31519b
ngocanhuit2005 <lengocanhpyne363@gmail.com>
2025-04-26 23:55:02 +0700

    feat(auth): add login, register, forgot password, and reset password pages with API integration

--- apps/frontend/app/auth/reset-password/page.tsx
+++ apps/frontend/app/auth/reset-password/page.tsx
@@ -1,13 +1,14 @@
 "use client";
+
 import NotFoundPage from "@/app/not-found";
 import ResetPasswordForm from "@/components/form/ResetPasswordForm";
 import ResetPasswordSuccess from "@/components/ResetPasswordSuccess";
 import { useAppStore } from "@/store";
 import { isValidJWT } from "@/utils";
 import { useSearchParams } from "next/navigation";
-import React from "react";
+import React, { Suspense } from "react";
 
-const ResetPasswordPage = () => {
+const ResetPasswordPageContent = () => {
   const searchParams = useSearchParams();
   const { isPasswordResetSuccess } = useAppStore();
 
@@ -16,38 +17,36 @@ const ResetPasswordPage = () => {
   if (
     !authorizationCode ||
     (authorizationCode && !isValidJWT(authorizationCode))
-  )
+  ) {
     return <NotFoundPage />;
+  }
 
   return (
-    <main
-      className="flex flex-col px-10 py-4 bg-white text-black h-screen mx-auto w-full items-center
-justify-center"
-    >
+    <main className="flex flex-col px-10 py-4 bg-white text-black h-screen mx-auto w-full items-center justify-center">
       {isPasswordResetSuccess ? (
-        <>
-          <ResetPasswordSuccess />
-        </>
+        <ResetPasswordSuccess />
       ) : (
-        <>
-          <div
-            className="flex flex-col justify-center items-center gap-8 md:w-1/2 w-full p-8 py-6
-  rounded-lg shadow-sm"
-          >
-            <div className="text-center w-full flex flex-col items-center justify-center">
-              <h1 className="md:text-2xl text-xl">Reset Your Password</h1>
-
-              <p className="md:text-sm text-[14px] text-black/60">
-                Please enter your new password and confirm it below to reset
-                your account password.
-              </p>
-            </div>
-
-            <ResetPasswordForm authorize_code={authorizationCode} />
+        <div className="flex flex-col justify-center items-center gap-8 md:w-1/2 w-full p-8 py-6 rounded-lg shadow-sm">
+          <div className="text-center w-full flex flex-col items-center justify-center">
+            <h1 className="md:text-2xl text-xl">Reset Your Password</h1>
+            <p className="md:text-sm text-[14px] text-black/60">
+              Please enter your new password and confirm it below to reset your
+              account password.
+            </p>
           </div>
-        </>
+
+          <ResetPasswordForm authorize_code={authorizationCode} />
+        </div>
       )}
     </main>
+  );
+};
+
+const ResetPasswordPage = () => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ResetPasswordPageContent />
+    </Suspense>
   );
 };
 
